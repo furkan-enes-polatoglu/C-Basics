24b13476c0bd8d45cc4f06c736b0dd3923430dee
Furkan Enes POLATOĞLU <furkanenes1160@gmail.com>
2017-12-30 01:22:41 +0300

    Add files via upload

---
+++ DaireAlaniHesaplama.c
@@ -0,0 +1,16 @@
+#include <stdio.h>;
+#include <conio.h>;
+
+void main()
+{
+
+    const float Pi=3.14;  //Float tipinde sabit deðiþken
+    float r,alan;         //Float tipinde r ve alan deðiþkenleri
+    r=7;                  
+    alan=Pi*r*r;
+    printf("Alan: %f",alan);
+    getch();              //Ekranda bekletir
+
+}
+
+
