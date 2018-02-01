4c1bd3ef83cab940969124d8683082e77281dd70
Furkan Enes POLATOĞLU <furkanenes1160@gmail.com>
2018-02-01 07:21:35 +0300

    Add files via upload

---
+++ C 1 (Giris Cikisi Fonksiyonlari)/Untitled1.cpp
@@ -0,0 +1,68 @@
+#include <locale.h> //Türkçe karakterler kullanmamýzý saðlayan kütüphane.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	setlocale(LC_ALL,"Turkish"); //Türkçe karakter kullanmak için yazýlan komut.
+	
+	/*
+	printf("Hello World!");
+	printf("\nBen bir\nC programcýsýyým."); // \n bir satýr aþaðý kaydýrýr.
+	printf("\nBenim adým\tFurkan Enes Polatoðlu."); // \t bir Tab boþluðu verir.
+	printf("\nProgram bitti, bana ses ver...\a"); //Uyarý mesajý ver a(Alert).
+	*/
+	
+	/*
+	int sayi=16;
+	int sayi2=4;
+	printf("%d , %d",sayi,sayi2); //sayýlarý yan yana yazdýr.
+	printf("\n%d\n%d",sayi,sayi2); //sayýlarý alt alta yazdýr.
+	*/
+	
+	/*
+	int sayi1=5,sayi2=10,sayi3=15;
+	printf("%d %d %d",sayi1,sayi2,sayi3);
+	printf("\n\n%d\n%d\n%d",sayi1,sayi2,sayi3);
+	*/
+	
+	/*
+	char ad[15] = "Furkan";
+	printf("%s",ad); //ad karakterini yazdýr.
+	printf("\n%c",ad[0]); //ad deðiþkenin ilk karakterini getir.
+	*/
+	
+	/*
+	char ad[15] = "Enes";
+	int yas = 19;
+	float boy = 1.78;
+	printf("AD:%s\nYAÞ:%d\nBOY:%.2f",ad,yas,boy); //%.2f virgülden sonra 2 basamak yazmasýný istedik.
+	*/
+	
+	/*
+	char isim[15];
+	int yas;
+	printf("Adýnýz : "); scanf("%s",&isim); //& adres operatörü anlamýna gelir.
+	printf("Yaþýnýz : "); scanf("%d",&yas);
+	printf("Benim adým %s, yaþým ise %d",isim,yas);
+	*/
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
